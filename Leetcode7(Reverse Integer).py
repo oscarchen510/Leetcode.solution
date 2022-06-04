0caa578f8985c4929b646f7c095a84342b295c54
oscarchen510 <81905686+oscarchen510@users.noreply.github.com>
2022-06-04 16:07:10 +0800

    Update Leetcode7(Reverse Integer).py

--- Leetcode7(Reverse Integer).py
+++ Leetcode7(Reverse Integer).py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def reverse(self, x: int) -> int:
+        total=0
+        sign = True #set a boolen to check if the number is negative
+        if x>0:
+            sign = True
+        else:
+            sign = False
+        x=abs(x)
+        while(x>0):
+            total=total*10 + x%10   #formula, to reverse a number
+            x=x//10
+        if total in range(-2**31, 2**31-1):
+            if sign == True:
+                return total
+            else:
+                return -total   #return number with '-'
+        else:
+            return 0
